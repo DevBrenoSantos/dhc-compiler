525f0cabcc90970161cb68a5c6d3b9411c1defed
Breno Amaral <contato.brenoamaral.s@gmail.com>
2025-08-23 15:23:40 -0300

    Primeira versão, colocando o leitor byte a byte e readme

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import functions.readFile;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        readFile.read(args);
     }
 }
